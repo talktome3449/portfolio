da6e72650e339f3c2604ca194505d97225e48b0f
Deepak Agrawal <agrawaldk1990@gmail.com>
2024-12-12 00:08:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ggggg';
 }
